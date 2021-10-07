52adeed69c7bcc57fd803bce350a836299f3efa4
Flavio Lima <flaviolima.s@live.com>
2021-10-07 11:57:57 -0300

    WIP - definindo rota para GET /ola

--- src/main/java/com/example/exerciciosspring/controllers/FirstController.java
+++ src/main/java/com/example/exerciciosspring/controllers/FirstController.java
@@ -8,7 +8,7 @@ public class FirstController {
 
     public static final String MESSAGE = "Hello Word!";
 
-    @GetMapping
+    @GetMapping(path = "/ola")
     public String hello() {
         return MESSAGE;
     }
