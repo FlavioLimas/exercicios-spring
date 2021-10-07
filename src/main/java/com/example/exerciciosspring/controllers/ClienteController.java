9eb9164d4a556e6fd38b9e966f0fe6e36de6b26c
Flavio Lima <flaviolima.s@live.com>
2021-10-07 17:57:36 -0300

    WIP - pegando parametro da URL

--- src/main/java/com/example/exerciciosspring/controllers/ClienteController.java
+++ src/main/java/com/example/exerciciosspring/controllers/ClienteController.java
@@ -4,6 +4,7 @@ import com.example.exerciciosspring.models.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +19,11 @@ public class ClienteController {
     @GetMapping("/{id}")
     public Cliente findById(@PathVariable int id) {
         return new Cliente(id, "Maria", "32143364016");
+    }
+
+    @GetMapping
+    public Cliente findByIdGetParamInUrl(@RequestParam(name = "id") int id) {
+        return new Cliente(id, "João", "50056303092");
     }
 
 }
