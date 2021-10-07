17892393df05243ecb8d5f0c0782c243b4cde4b8
Flavio Lima <flaviolima.s@live.com>
2021-10-07 14:48:02 -0300

    WIP - alterando rota do servico para mapear a classe para representar clientes

--- src/main/java/com/example/exerciciosspring/controllers/ClienteController.java
+++ src/main/java/com/example/exerciciosspring/controllers/ClienteController.java
@@ -2,12 +2,14 @@ package com.example.exerciciosspring.controllers;
 
 import com.example.exerciciosspring.models.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "/clientes")
 public class ClienteController {
 
-    @GetMapping(path = "/clientes/qualquer")
+    @GetMapping(path = "/qualquer")
     public Cliente getCliente() {
         return new Cliente(28, "Pedro", "76823380080");
     }
