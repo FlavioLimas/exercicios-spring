bd25dff22b6d86851e769049d2ff912fb3cfd4cb
Flavio Lima <flaviolima.s@live.com>
2021-10-07 11:37:31 -0300

    WIP - anotando classe com restController

--- src/main/java/com/example/exerciciosspring/controllers/FirstController.java
+++ src/main/java/com/example/exerciciosspring/controllers/FirstController.java
@@ -1,5 +1,8 @@
 package com.example.exerciciosspring.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class FirstController {
 
     public static final String MESSAGE = "Hello Word!";
