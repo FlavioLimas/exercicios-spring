0061ecdf7089e7474a25bfc5444e870783e6ad11
Flavio Lima <flaviolima.s@live.com>
2021-10-07 17:31:53 -0300

    WIP - findById

--- src/main/java/com/example/exerciciosspring/controllers/ClienteController.java
+++ src/main/java/com/example/exerciciosspring/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package com.example.exerciciosspring.controllers;
 
 import com.example.exerciciosspring.models.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,11 @@ public class ClienteController {
     @GetMapping("/qualquer")
     public Cliente getCliente() {
         return new Cliente(28, "Pedro", "76823380080");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente findById(@PathVariable int id) {
+        return new Cliente(id, "Maria", "32143364016");
     }
 
 }
