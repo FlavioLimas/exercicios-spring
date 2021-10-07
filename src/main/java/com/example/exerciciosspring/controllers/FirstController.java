9706cd160b9dd4417a078f3a1fd3d691d609bbe0
Flavio Lima <flaviolima.s@live.com>
2021-10-07 11:45:14 -0300

    WIP - definindo mapeamento rest

--- src/main/java/com/example/exerciciosspring/controllers/FirstController.java
+++ src/main/java/com/example/exerciciosspring/controllers/FirstController.java
@@ -1,5 +1,6 @@
 package com.example.exerciciosspring.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -7,6 +8,7 @@ public class FirstController {
 
     public static final String MESSAGE = "Hello Word!";
 
+    @RequestMapping
     public String hello() {
         return MESSAGE;
     }
