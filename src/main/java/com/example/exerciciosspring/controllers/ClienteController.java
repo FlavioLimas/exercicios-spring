6fe6c3e4eed2b2f28dbf70ddc018977058c0d133
Flavio Lima <flaviolima.s@live.com>
2021-10-07 12:59:27 -0300

    WIP - anotando classe e fefinindo rota

--- src/main/java/com/example/exerciciosspring/controllers/ClienteController.java
+++ src/main/java/com/example/exerciciosspring/controllers/ClienteController.java
@@ -1,9 +1,13 @@
 package com.example.exerciciosspring.controllers;
 
 import com.example.exerciciosspring.models.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ClienteController {
 
+    @GetMapping(path = "/clientes/qualquer")
     public Cliente getCliente() {
         return new Cliente(28, "Pedro", "76823380080");
     }
