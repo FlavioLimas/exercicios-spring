8d044423195a8d64409bb7b47743691e3d38847f
Flavio Lima <flaviolima.s@live.com>
2021-10-07 12:52:10 -0300

    WIP - chamando o construtor da classe pai

--- src/main/java/com/example/exerciciosspring/models/Cliente.java
+++ src/main/java/com/example/exerciciosspring/models/Cliente.java
@@ -1,5 +1,10 @@
 package com.example.exerciciosspring.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Cliente {
 
     private int id;
@@ -7,6 +12,7 @@ public class Cliente {
     private String cpf;
 
     public Cliente(int id, String nome, String cpf) {
+        super();
         this.id = id;
         this.nome = nome;
         this.cpf = cpf;
