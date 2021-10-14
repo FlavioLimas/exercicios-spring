5be06f6ff4bcb1fe7d0b27ada3f8b3b0de9996a3
Flavio Lima <flaviolima.s@live.com>
2021-10-14 20:44:20 -0300

    WIP - anotando entity

--- src/main/java/com/example/exerciciosspring/model/entity/Produto.java
+++ src/main/java/com/example/exerciciosspring/model/entity/Produto.java
@@ -1,7 +1,19 @@
 package com.example.exerciciosspring.model.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@Getter
+@Setter
+@Entity
 public class Produto {
 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String nome;
 }
