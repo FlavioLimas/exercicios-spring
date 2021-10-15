910cfb7bd8b5b461778455acb1e1516c58c24e9d
Flavio Lima <flaviolima.s@live.com>
2021-10-14 21:25:37 -0300

    WIP - ajuste

--- src/main/java/com/example/exerciciosspring/controllers/ProdutoController.java
+++ src/main/java/com/example/exerciciosspring/controllers/ProdutoController.java
@@ -4,14 +4,15 @@ import com.example.exerciciosspring.model.entity.Produto;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/produtos")
 public class ProdutoController {
 
-    @PostMapping("")
-    public Produto createProduct(@RequestParam String name) {
+    @PostMapping
+    public @ResponseBody Produto createProduct(@RequestParam String name) {
         return new Produto(name);
     }
 }
