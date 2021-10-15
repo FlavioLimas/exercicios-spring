8c3fba776c17c10f3e07f1c05a616d5a67092624
Flavio Lima <flaviolima.s@live.com>
2021-10-14 21:30:13 -0300

    WIP - new class

--- src/main/java/com/example/exerciciosspring/model/entity/Product.java
+++ src/main/java/com/example/exerciciosspring/model/entity/Product.java
@@ -1,24 +1,20 @@
 package com.example.exerciciosspring.model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
-@Getter
-@Setter
 @Entity
-public class Produto {
+public class Product {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
 
-    public Produto(String nome) {
+    public Product(String nome) {
         this.name = nome;
     }
 
 }
+
