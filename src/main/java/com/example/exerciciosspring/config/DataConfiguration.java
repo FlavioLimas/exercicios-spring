2625344676c141652be76dc7cda8051193de0b5e
Flavio Lima <flaviolima.s@live.com>
2021-11-22 21:15:58 -0300

    WIP - Classe de configuracao do bean

---
+++ src/main/java/com/example/exerciciosspring/config/DataConfiguration.java
@@ -0,0 +1,38 @@
+package com.example.exerciciosspring.config;
+
+import org.hibernate.Hibernate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
+@Component
+public class DataConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.mariadb.jdbc.Driver");
+        dataSource.setUrl("jdbc:mariadb://localhost:3306/springboot");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setDatabase(Database.MYSQL);
+        adapter.setGenerateDdl(Boolean.TRUE);
+        adapter.setShowSql(Boolean.TRUE);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MariaDB103Dialect");
+        adapter.setPrepareConnection(Boolean.TRUE);
+        return adapter;
+    }
+}
